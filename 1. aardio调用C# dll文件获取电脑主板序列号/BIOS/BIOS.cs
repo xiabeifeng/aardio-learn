38db0d7a5a67bc65e535863b9db5aee25591c8db
夏明 <840679824@qq.com>
2021-08-29 17:35:37 +0800

    aardio调用C# dll文件获取电脑主板序列号

---
+++ 1. aardio调用C# dll文件获取电脑主板序列号/BIOS/BIOS.cs
@@ -0,0 +1,29 @@
+﻿using System.Management;
+
+namespace BIOS
+{
+    public class BIOS
+    {
+
+        // 获取主板序列号
+        public string GetBIOSSerialNumber()
+        {
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("Select * From Win32_BIOS");
+                string serialNumber = "";
+                foreach (ManagementObject info in searcher.Get())
+                {
+                    serialNumber = info["SerialNumber"].ToString().Trim();
+                }
+
+                return serialNumber;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+    }
+}
